5fb6fc3a3c9be9f8e94ebbc09353f92fb6ca27bb
TianxinSun <93703023+TianxinSun@users.noreply.github.com>
2023-08-06 10:42:23 +0800

    Sammuny

--- src/views/college-profile/collage/maps-page/components/maps-pages-components-index.vue
+++ src/views/college-profile/collage/maps-page/components/maps-pages-components-index.vue
@@ -3,9 +3,19 @@
     <div v-for="item in imgList" :key="item.name">
       <template v-if="activeLogoTabs === item.name">
         <div class="lots-img-box" v-if="item.children">
-          <div v-for="i in item.children" :key="i.name">
+          <div v-for="i in newData" :key="i.name">
             <img class="lots-img" :src="i.src" />
             <span>{{i.name}}</span>
+          </div>
+          <div class="example-pagination-block">
+            <div class="example-demonstration">When you have few pages</div>
+            <el-pagination
+              layout="prev, pager, next"
+              :total="changePage.total"
+              @current-change="handleCurrentChange"
+              v-model:current-page="changePage.currentPage"
+              :page-size="changePage.pageSize"
+            />
           </div>
         </div>
         <div class="single-img-box" v-else>
@@ -36,15 +46,49 @@ export default {
     const fatherKey = ref(props.key)
     const activeLogoTabs = ref(props.changeLogoTabs)
     const imgList = reactive(props.imgData)
+    const newData = ref([])
+    const totalDataList = reactive([])
+    const changePage = reactive({
+      currentPage: 1,
+      total: totalDataList.length,
+      pageSize: 10
+    })
 
     watch(props, () => {
       activeLogoTabs.value = props.changeLogoTabs
+      handleCurrentChange(1)
     })
+
+    const handleCurrentChange = (value) => {
+      console.log(value)
+      changePage.currentPage = value
+      for (const subItem of imgList) {
+        if (subItem.children) {
+          totalDataList.value = subItem.children
+          let i = value * 10 - 10
+          const array = []
+          while (i < changePage.currentPage * 10) {
+            if (subItem.children[i] != null) {
+              array.push(subItem.children[i])
+              i++
+              continue
+            }
+            break
+          }
+          newData.value = array
+        } else {
+          newData.value = subItem.children
+        }
+      }
+    }
 
     return {
       activeLogoTabs,
       imgList,
-      fatherKey
+      fatherKey,
+      changePage,
+      newData,
+      handleCurrentChange
     }
   }
 }
